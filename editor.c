627c2ae9a7715559889c226eed4f8f1c222175a2
Divyaank Tiwari <divyaankt21@gmail.com>
2020-04-30 20:28:19 +0530

    Disable raw mode at exit

--- editor.c
+++ editor.c
@@ -1,11 +1,17 @@
 #include <termios.h>
 #include <unistd.h>
+#include <stdlib.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 void enableRawMode() {
-	struct termios raw;
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
 
-	tcgetattr(STDIN_FILENO, &raw);
-
+	struct termios raw = orig_termios;
 	//c_lflag misc flag
 	//ECHO is a bitflag, defined as 00000000000000000000000000001000 
 	raw.c_lflag &= ~(ECHO);
