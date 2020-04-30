515f720471c7d7b896a0a25edb1f64e12981568b
Divyaank Tiwari <divyaankt21@gmail.com>
2020-04-30 16:59:38 +0530

    Turn off echoing by fiddling with termina attributes

--- editor.c
+++ editor.c
@@ -1,9 +1,20 @@
+#include <termios.h>
 #include <unistd.h>
+
+void enablerawMode() {
+	struct termios raw;
+
+	tcgettattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcgettattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 int main() {
 
 	char c;
 
-	while(read(STDIN_FILENO, &c, 1) == 1 && c!='q');
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
 	return 0;
 }
