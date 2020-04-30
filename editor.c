261a2721d7f98331304447ef6112d6c0e9487f68
Divyaank Tiwari <divyaankt21@gmail.com>
2020-04-30 22:42:28 +0530

    Turned-off some miscellaneous flags

--- editor.c
+++ editor.c
@@ -20,12 +20,16 @@ void enableRawMode() {
 	//Ctrl + M should actually be read as 13, whereas we find it to be 10
 	//Terminal is helpfully translating any carriage returns (13, '\r') inputted by the user into newlines (10, '\n').
 	//ICRNL fixes this behavious CR-Carriage Return NL-Newline
-	raw.c_iflag &= ~(ICRNL | IXON);
+	//Rest are misc flags
+	raw.c_iflag &= ~(ICRNL | IXON | ISTRIP | INPCK | BRKINT);
 
 	//c_oflag is an output flag
 	//Terminal translates "\n" to "\r\n", this is required for start of newline
 	//turn-off output processing by OPOST flag
 	raw.c_oflag &= ~(OPOST);
+
+	//Misc Flag
+	raw.c_cflag |= (CS8);
 
 	//c_lflag misc flag
 	//ICANON is a flag which is responsible for Canonical Mode
@@ -45,9 +49,11 @@ int main() {
 		if (iscntrl(c)) {
 			//Block is executed when c is a control character
 			//ASCII 0-31 as well 127 are all control characters
+			//Without \r, the output shifts rightward constantly at newline
 			printf("%d\r\n", c);
 		}
 		else{
+			//Without \r, the output shifts rightward constantly at newline
 			printf("%d ('%c')\r\n", c, c);
 		}
 	}
